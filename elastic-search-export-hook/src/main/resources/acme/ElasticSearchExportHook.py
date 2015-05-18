e2171ed5691fd6daffad8634b24ee15e0dc794ab
Mike Kotsur <miccots@gmail.com>
2015-05-18 16:15:52 +0200

    Fixed call to urllib2

--- elastic-search-export-hook/src/main/resources/acme/ElasticSearchExportHook.py
+++ elastic-search-export-hook/src/main/resources/acme/ElasticSearchExportHook.py
@@ -1,23 +1,27 @@
 import urllib2
 
 # Small helper to make a POST request
-def publish_to_elastic_search(uri,payload):
+def publish_to_elastic_search(uri, payload):
     logger.debug('Posting to URI %s' % uri)
     req = urllib2.Request(uri, payload)
-    return urllib2.urlopen(req)
+    try:
+        response = urllib2.urlopen(req)
+    except urllib2.HTTPError, e:
+        if e.code == 201:
+            logger.debug('The release has been pushed into ES')
+        else:
+            logger.error('Could not push release into ES', e)
+            raise e
+
 
 # Logger object of type `org.slf4j.Logger` is available.
 # Check http://www.slf4j.org/apidocs/org/slf4j/Logger.html for more details.
 # You can set custom logging level by using referring to the name: `com.xebialabs.platform.script.Logging`.
 logger.info("Starting export of the release %s v2" % release.id)
 
-response = publish_to_elastic_search(
+publish_to_elastic_search(
     # `exportHook` object provides access to properties defined at `acme.ElasticSearchExportHook` synthetic type.
     uri = '%s/%s/%s' % (exportHook.url, exportHook.typeName, exportHook.indexName),
     # `releaseJson` contains a string with serialized release.
     payload = releaseJson
 )
-
-logger.debug('The response code was: %s' % response.code)
-logger.debug('The response body was: %s' % response.read())
-assert response.code <= 300
